9f05fe318e419ff2e607507ceee22c63b4691e88
ahmadabudames <ahmadabudamesmais1999@gmail.com>
2021-09-07 21:09:22 +0300

    finished lab32

--- blogapi_project/settings.py
+++ blogapi_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'rest_framework',
+    'rest_framework',
     'post',
 ]
 
@@ -133,3 +133,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' : [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
--- post/views.py
+++ post/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Post
+from .serializers import PostSerializer
+from .permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+class PostList(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
